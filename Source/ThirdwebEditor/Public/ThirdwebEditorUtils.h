bfc5cd7f55bf095fa213a90c78aa2f262b6bbba5
Firekeeper <0xFirekeeper@gmail.com>
2025-02-14 01:19:46 +0700

    fix mac compilation

--- Source/ThirdwebEditor/Public/ThirdwebEditorUtils.h
+++ Source/ThirdwebEditor/Public/ThirdwebEditorUtils.h
@@ -2,8 +2,19 @@
 
 #pragma once
 
+#include "ISettingsModule.h"
+#include "Modules/ModuleManager.h"
+
 namespace ThirdwebEditorUtils
 {
 	template <typename T = UDeveloperSettings>
-	extern void OpenDeveloperSettings();
+	void OpenDeveloperSettings()
+	{
+		const T* Settings = GetDefault<T>();
+		FModuleManager::LoadModuleChecked<ISettingsModule>("Settings").ShowViewer(
+			Settings->GetContainerName(), 
+			Settings->GetCategoryName(), 
+			Settings->GetSectionName()
+		);
+	}
 }
